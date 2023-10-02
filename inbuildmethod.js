96aea9bcc0880c2f3321f5d2091c936e0fcb7fae
Sanketsubedi <itsmesanketsubedi@gmail.com>
2023-10-02 13:49:54 +0545

    learned bout some of the in build methods

---
+++ inbuildmethod.js
@@ -0,0 +1,52 @@
+// pre-existing function are known as in Build Method
+
+// eg setTimeout
+
+// var name = 'Sanket Subedi   ';
+// console.log('Length>>>',name.length);
+// console.log('Uppercase>>>',name.toUpperCase());
+// console.log('Length>>>',name.trim().length);
+// console.log('string>>>',name.substr(0,4));
+// console.log('string>>>',name.substring(0,4));//in between doesn't count last
+
+
+// type conversion
+// var games= 'CSGO,Valorant,CS2';
+// console.log('into array>>>',games.split(',')); 
+
+// var num = 555.564555
+// var fixedeci = num.toFixed(2); // output>> 555.56
+// var intonly = parseInt(num); // output>> 555
+// var type = typeof(num);
+
+// num = 555
+// num2 = '555'
+
+// num == num2 //true
+// num === num2 // false
+
+
+// var a = num + num2//concate garxa
+// var a = num + num2//multiplies 
+// Number(num2) // converts into number from str
+// isNan(a); // check if number or not and returns boolean
+
+// object
+// var obj = {
+// 	name: 'Sanket',
+// 	address: 'Bharatpur',
+// 	phone: 9854545656
+// };
+// obj.hasOwnProperty('name');
+
+// //obj to array
+// var keysonly = Object.keys(obj);
+// var valuesonly = Object.values(obj);
+// console.log('keys:',keysonly);
+// console.log('values:',valuesonly);
+
+// obj to string
+// var tostr = JSON.stringify(obj);
+ 
+//  str to object
+//  var obj = JSON.parse(tostr);
