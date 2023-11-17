6d26b7e11bc1977fc420b71242076ebbeb95170b
Sanketsubedi <itsmesanketsubedi@gmail.com>
2023-11-17 14:29:07 +0545

    created modules

---
+++ node/1-names.js
@@ -0,0 +1,5 @@
+//share
+const name1 = 'Sanket'
+const name2 = 'Harry'
+
+module.exports= {name1,name2}
---
+++ node/app.js
@@ -0,0 +1,6 @@
+const names = require('./1-names')
+const funcs = require('./2-fun')
+
+// console.log(funcs)
+funcs(names.name1)
+funcs(names.name2)
