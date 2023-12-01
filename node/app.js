024e43ddb3329b2c07c272fb7d959a107cd859da
Sanketsubedi <itsmesanketsubedi@gmail.com>
2023-12-01 16:11:41 +0545

    push

--- node/app.js
+++ node/app.js
@@ -1,7 +1,10 @@
-const names = require('./1-names')
-const funcs = require('./2-fun')
+// const names = require('./1-names')
+// const funcs = require('./2-fun')
 
-// console.log(funcs)
-funcs(names.name1)
-funcs(names.name2)
-// for push
+// // console.log(funcs)
+// funcs(names.name1)
+// funcs(names.name2)
+// // for push
+
+const path = require('path')
+console.log(path.sep)
